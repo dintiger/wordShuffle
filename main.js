a839bfd7af2a7dbeced74fd22e3c97c20d1d85ac
dintiger <dintiger17@gmail.com>
2023-05-26 14:47:47 +0800

    finalProduct

---
+++ main.js
@@ -0,0 +1,197 @@
+// // dont show repeated  words
+
+// words[done].done = "done";
+// console.log(words);
+
+// grab html data !
+const wordText = document.querySelector(".word");
+const hintText = document.querySelector(".hint");
+const lifeText = document.querySelector(".life");
+const skipButton = document.querySelector(".skip-word");
+const scoreText = document.querySelector(".score");
+const timerCount = document.querySelector(".timer");
+const inputText = document.querySelector(".input-guess");
+const submitAnswer = document.querySelector(".submit-word");
+const levelText = document.querySelector(".level");
+const canvas = document.querySelector("#confetti");
+
+// initial variable
+let word = "";
+let timer;
+let time = 15;
+let playerScore = 0;
+let playerLife = 3;
+let playerEmoji = "❤️❤️❤️";
+let randomWord = {};
+let gameLevel = "Level 1";
+const jsConfetti = new JSConfetti();
+
+function startGame() {
+  inputText.value = "";
+  //filter the words is user tried it & pick random word
+  const filteredWords = words.filter((obj) => !obj.done);
+  let randomIndex = Math.floor(Math.random() * filteredWords.length);
+  let randomWord = filteredWords[randomIndex];
+  randomWord.done = true;
+
+  console.log(filteredWords);
+
+  word = randomWord.word.toLowerCase();
+  let wordArr = word.split("").sort(() => Math.random() - 0.6);
+  let shuffleWord = wordArr.join(" ");
+  console.log(word);
+
+  lifeText.innerHTML = `Life: ${playerEmoji}`;
+  hintText.innerHTML = `hint: ${randomWord.hint}`;
+  wordText.innerHTML = shuffleWord;
+  scoreText.innerHTML = `SCORE: ${playerScore}`;
+  timerCount.innerHTML = time;
+  levelText.innerHTML = gameLevel;
+  // console.log(word);
+  // console.log("game start");
+
+  // timer = setInterval(() => {
+  //   if (time > 0) {
+  //     time--;
+  //     return (timerCount.innerHTML = time);
+  //   }
+  //   loseGame();
+  // }, 1000);
+}
+
+function rightAnswer() {
+  jsConfetti.addConfetti();
+  playerScore++;
+  time = 15;
+  clearInterval(timer);
+  startGame();
+  if (playerScore > 5) {
+    levelUp();
+  }
+  if (playerScore >= 100) {
+    const choice = prompt(
+      `You win the game sah!! You score: ${playerScore}, Restart GAME?`,
+      "yes"
+    );
+    if (choice.toLowerCase() == "yes") {
+      gameRestart();
+      console.log("game restart");
+    }
+  }
+}
+
+function levelUp() {
+  // if player score more than ... ,
+  // cut time to half
+  console.log("levelup");
+  if (playerScore > 5);
+  {
+    gameLevel = "Level 2";
+    time = 8;
+    clearInterval(timer);
+    startGame();
+  }
+}
+
+function loseGame() {
+  jsConfetti.addConfetti({
+    emojis: ["💩", "🤬", "😩", "💩", "🥶", "💩"],
+  });
+  playerLife--;
+  playerEmoji = playerEmoji.slice(0, -1);
+  playerEmoji = playerEmoji.slice(0, -1);
+
+  time = 15;
+
+  clearInterval(timer);
+  if (playerScore > 3) {
+    levelUp();
+  }
+  startGame();
+  if (playerLife < 0) {
+    const choice = prompt(
+      `Game Over, u ran out of life!!, You score: ${playerScore}, Restart GAME?`,
+      "yes"
+    );
+    if (choice.toLowerCase() == "yes") {
+      gameRestart();
+      console.log("game restart");
+    }
+  }
+}
+
+// input answer && skip button
+let answer = "";
+skipButton.addEventListener("click", () => loseGame());
+inputText.addEventListener("input", (event) => {
+  answer = event.target.value;
+});
+
+submitAnswer.addEventListener("click", () => {
+  compareAnswer();
+  // only mark xxx if u submit it, wont xxx if u skip the word
+  // randomWord.done = "xxx";
+});
+
+// submit answer using "enter"
+inputText.addEventListener("keydown", (event) => {
+  if (event.key === "Enter") {
+    event.preventDefault(); // Prevent the default form submission behavior
+    compareAnswer(answer);
+    console.log(event.key);
+    // randomWord.done = "xxx";
+  }
+});
+
+//
+function compareAnswer() {
+  // console.log(answer);
+  // console.log(word);
+  randomWord.done = "xxx";
+
+  if (answer === word) {
+    console.log("Correct!");
+
+    rightAnswer();
+    // Perform actions for correct guess
+  } else {
+    console.log("Wrong");
+    loseGame();
+    // Perform actions for wrong guess
+  }
+}
+
+function skipGame() {
+  jsConfetti.addConfetti({
+    emojis: ["💩", "🤬", "😩", "💩", "🥶", "💩"],
+  });
+  if (playerScore > 3) {
+    levelUp();
+  }
+  word = "";
+  time = 15;
+  clearInterval(timer);
+  startGame();
+}
+
+function gameRestart() {
+  word = "";
+  let gameLevel = "Level 1";
+  time = 15;
+  playerScore = 0;
+  playerLife = 3;
+  playerEmoji = "❤️❤️❤️";
+  clearInterval(timer);
+  startGame();
+}
+// function gameOver() {
+//   if ((playerLife = -1));
+//   alert("Game Over, u ran out of life!!, restart game?");
+//   word = "";
+//   time = 10;
+//   playerLife = 3;
+//   clearInterval(timer);
+//   startGame();
+// }
+
+startGame();
---
+++ words.js
@@ -0,0 +1,185 @@
+const words = [
+  { word: "building", hint: "concrete jungle" },
+  { word: "jungle", hint: "flora & fauna" },
+  { word: "airport", hint: "alot of passengers" },
+  {
+    word: "solemnize",
+    hint: "to observe or commemorate with rites or ceremonies",
+  },
+  { word: "apple", hint: "round fruit with red or green skin" },
+  { word: "cat", hint: "small domesticated carnivorous mammal" },
+
+  { word: "tree", hint: "tall plant with trunk and branches made of wood" },
+  { word: "car", hint: "road vehicle with an engine" },
+  { word: "dog", hint: "domesticated mammal with acute sense of smell" },
+  {
+    word: "computer",
+    hint: "electronic device for processing and storing data",
+  },
+  { word: "guitar", hint: "stringed musical instrument played with fingers" },
+  { word: "house", hint: "building for human habitation" },
+
+  { word: "mountain", hint: "large natural elevation with steep sides" },
+
+  { word: "rainbow", hint: "meteorological phenomenon with colors" },
+
+  {
+    word: "book",
+    hint: "a written or printed work consisting of pages glued or sewn together along one side",
+  },
+  {
+    word: "sun",
+    hint: "the star around which the earth orbits, providing light and heat",
+  },
+
+  {
+    word: "ocean",
+    hint: "vast body of saltwater covering most of the Earth's surface",
+  },
+  { word: "pencil", hint: "writing instrument with graphite or colored lead" },
+
+  { word: "flower", hint: "colorful bloom of a plant" },
+  { word: "butterfly", hint: "insect with colorful wings and a slender body" },
+
+  {
+    word: "coffee",
+    hint: "popular caffeinated beverage made from roasted beans",
+  },
+  { word: "bicycle", hint: "two-wheeled vehicle propelled by pedals" },
+
+  { word: "beach", hint: "sandy shore by the ocean or a lake" },
+
+  {
+    word: "camera",
+    hint: "device used to capture photographs or record videos",
+  },
+
+  { word: "garden", hint: "outdoor space cultivated with plants and flowers" },
+  {
+    word: "ballet",
+    hint: "form of dance characterized by grace and precision",
+  },
+  { word: "clock", hint: "device used to measure and display time" },
+
+  { word: "cloud", hint: "visible mass of condensed water vapor in the sky" },
+  {
+    word: "soccer",
+    hint: "popular sport played with a round ball and two teams",
+  },
+
+  { word: "mirror", hint: "reflective surface that shows an image" },
+  {
+    word: "sunglasses",
+    hint: "eyewear designed to protect the eyes from sunlight",
+  },
+  { word: "breeze", hint: "gentle wind or current of air" },
+  {
+    word: "painting",
+    hint: "artistic creation made with pigments on a surface",
+  },
+  { word: "birthday", hint: "anniversary of the day a person was born" },
+  { word: "raindrop", hint: "single drop of rain falling from the sky" },
+  { word: "swimming", hint: "activity of propelling oneself through water" },
+  {
+    word: "sunset",
+    hint: "the daily disappearance of the sun below the horizon",
+  },
+  {
+    word: "mountain",
+    hint: "a large natural elevation of the earth's surface",
+  },
+  {
+    word: "ocean",
+    hint: "a vast body of saltwater that covers most of the Earth",
+  },
+  {
+    word: "pencil",
+    hint: "a writing instrument with a thin cylindrical graphite core",
+  },
+  {
+    word: "music",
+    hint: "an art form that uses sound and rhythm to express emotions",
+  },
+  { word: "beach", hint: "a sandy or pebbly shore by the ocean, sea, or lake" },
+  {
+    word: "fireworks",
+    hint: "a display of colorful explosives used for celebration",
+  },
+
+  {
+    word: "garden",
+    hint: "a plot of land where plants, flowers, or vegetables are cultivated",
+  },
+
+  {
+    word: "chocolate",
+    hint: "a sweet, typically brown food made from cacao beans",
+  },
+  { word: "raincoat", hint: "a waterproof coat worn to protect against rain" },
+  { word: "globe", hint: "a spherical model of the Earth or celestial body" },
+  {
+    word: "jazz",
+    hint: "a genre of music characterized by improvisation and syncopation",
+  },
+
+  { word: "book", hint: "a written or printed work consisting of pages" },
+  {
+    word: "sailing",
+    hint: "the act of navigating or traveling on water using sails",
+  },
+  { word: "lamp", hint: "a portable light source with a bulb and a shade" },
+  { word: "adventure", hint: "an exciting or daring experience or activity" },
+  { word: "camera", hint: "a device used to capture and record visual images" },
+  { word: "piano", hint: "a musical instrument played by pressing keys" },
+  { word: "hiking", hint: "the activity of walking in natural environments" },
+
+  {
+    word: "art",
+    hint: "the expression or application of human creative skill and imagination",
+  },
+  { word: "basketball", hint: "a team sport played with a ball and two hoops" },
+  {
+    word: "tea",
+    hint: "a hot or cold beverage made by infusing dried leaves in water",
+  },
+  {
+    word: "guitar",
+    hint: "a stringed musical instrument played with fingers or a pick",
+  },
+  {
+    word: "space",
+    hint: "the vast expanse that exists beyond Earth's atmosphere",
+  },
+  {
+    word: "fireplace",
+    hint: "a structure where a fire can be made for heating or cooking",
+  },
+  { word: "dance", hint: "a series of rhythmic movements performed to music" },
+  {
+    word: "computer",
+    hint: "an electronic device for storing and processing data",
+  },
+  { word: "beach", hint: "a sandy or pebbly shore by the ocean, sea, or lake" },
+  {
+    word: "moon",
+    hint: "the natural satellite of the Earth, visible in the night sky",
+  },
+  {
+    word: "volcano",
+    hint: "an opening in Earth's crust that allows hot lava and gases to escape",
+  },
+
+  {
+    word: "painting",
+    hint: "an artistic creation made with colors on a surface",
+  },
+
+  {
+    word: "pizza",
+    hint: "a savory dish consisting of a round, flattened dough topped with ingredients",
+  },
+  {
+    word: "jungle",
+    hint: "a dense tropical forest with lush vegetation and wildlife",
+  },
+];
